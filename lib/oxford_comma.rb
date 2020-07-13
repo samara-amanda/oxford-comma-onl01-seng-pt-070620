daea70b6e41924b5bc01e80de73ffda11207f785
Samara Grant <samara.amanda01@gmail.com>
2020-07-13 13:31:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length == 3
-    array[0,1].join(", ")
-    array[2].join(" and ")
+  elsif array.length >= 3
+    array.join(", ")
   end
 end
